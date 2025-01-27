9e7804245a6227ac6399af5f0b7a300a38805d47
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2025-01-28 01:29:20 +0530

    Important very : understand it , little difficult but take reference from cart controller, service,schema

--- src/address/schemas/address.schema.ts
+++ src/address/schemas/address.schema.ts
@@ -4,7 +4,7 @@ import { Types } from 'mongoose';
 import { User } from 'src/user/schemas/user.schema';
 import * as dotenv from 'dotenv';
 import debugLib from 'debug'
-const log = debugLib('app:address schema')
+const log = debugLib('app:address:schema')
 
 // Load environment variables
 dotenv.config();
@@ -12,32 +12,32 @@ dotenv.config();
 const isDevelopment = process.env.NODE_ENV === 'development';
 log("node env development or not",isDevelopment)
 
-@Schema({ timestamps: true })
+@Schema()
 export class Address extends Document {
-  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true }) // Reference to User model
   userId: Types.ObjectId;
 
-  @Prop({ required: true })
-  street: string;
-
-  @Prop({ required: true })
-  city: string;
-
-  @Prop({ required: true })
-  state: string;
-
-  @Prop({ required: true })
-  country: string;
-
-  @Prop({ required: true })
-  postalCode: string;
-
-  @Prop({ default: false })
-  isDefault: boolean;
-
-  // Conditionally add the userEmail field in development mode
-  @Prop(isDevelopment ? { required: false } : undefined)
+  @Prop({ type: String, required: false }) // Include user email optionally
   userEmail?: string;
+
+  @Prop([
+    {
+      street: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      country: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      isDefault: { type: Boolean, default: false }, // Indicates if this is the default address
+    },
+  ])
+  addresses: Array<{
+    street: string;
+    city: string;
+    state: string;
+    country: string;
+    postalCode: string;
+    isDefault?: boolean;
+  }>;
 }
 
 export const AddressSchema = SchemaFactory.createForClass(Address);
