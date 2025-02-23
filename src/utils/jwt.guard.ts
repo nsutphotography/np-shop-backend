64e93050b83530a214e608d02f307f4460633df3
nsutphotography <nsutphotography@gmail.com>
2025-02-23 13:06:18 +0530

    enhance JWT authentication guard: improve error handling for expired tokens and unauthorized access

--- src/utils/jwt.guard.ts
+++ src/utils/jwt.guard.ts
@@ -1,16 +1,22 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { TokenExpiredError } from 'jsonwebtoken';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  handleRequest(err, user, info) {
-    console.log('AuthGuard Error:', err); // Log any errors
-    console.log('AuthGuard User:', user); // Log the user object
-    console.log('AuthGuard Info:', info); // Log the additional info from Passport
+  handleRequest(err: any, user: any, info: any) {
+    console.log('AuthGuard Error:', err);
+    console.log('AuthGuard User:', user);
+    console.log('AuthGuard Info:', info);
 
-    if (err || !user) {
-      console.error('Unauthorized: Error or user not found');
-      throw err || new UnauthorizedException();
+    if (err || info instanceof TokenExpiredError) {
+      console.error('Unauthorized: Token expired or error encountered', err || info);
+      throw new UnauthorizedException(info?.message || 'Authentication error');
+    }
+    
+    if (!user) {
+      console.error('Unauthorized: User not found');
+      throw new UnauthorizedException('User not found');
     }
 
     return user;
